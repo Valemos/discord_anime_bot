2d43a9369adb9b2b1f8ea0a17a9c041d11901746
Valemos <programworkerbox@gmail.com>
2021-01-03 22:35:11 +0200

    created test bot

--- src/main/java/BotDiscordStart.java
+++ src/main/java/BotDiscordStart.java
@@ -1,7 +1,12 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 
 public class BotDiscordStart {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        System.out.println("init");
+        JDA jda = JDABuilder.createDefault("Nzk1MzU0NDIzMTk3OTU4MTY0.X_IJhw.t59wu6ie-UZ7xPw8DW-HOxcl3sk").build();
+        System.out.println(jda.getStatus().toString());
     }
 }
